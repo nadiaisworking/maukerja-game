ac2f79d3f1b4945e99614a78530d644b71669197
nadiaisworking <nadia@ajobthing.com>
2026-01-22 11:35:01 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// VERSION 4.0 - FORCE UPDATE (Database Fix)
+// VERSION 5.0 - FORCE UPDATE (Final Polish)
 document.addEventListener('DOMContentLoaded', () => {
     // DOM Elements
     const startOverlay = document.getElementById('start-overlay');
